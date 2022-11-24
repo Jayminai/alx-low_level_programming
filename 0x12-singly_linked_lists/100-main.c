821c10d7fa357099eb8709109446d21e41f214ff
Jayminai <georgegodstime62@gmail.com>
2022-11-24 08:21:04 +0100

    Singly link lists

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */ int main(void)
+{
+
+printf("(A rat having a sense of a hare's nature, challenge a race.)\n");
+return (0);
+
+}
